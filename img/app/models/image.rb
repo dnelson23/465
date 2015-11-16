06f043a4173e46118be92f69bb801c5dda757689
Drew Nelson <drwnlsn@gmail.com>
2015-11-15 22:10:13 -0800

    Adds image, image_user, and tag models

---
+++ img/app/models/image.rb
@@ -0,0 +1,10 @@
+class Image < ActiveRecord::Base
+    require 'digest'
+
+    validates :filename, presence: true
+
+    belongs_to :user
+    has_many :users, through: :image_users 
+    has_many :tags
+
+end
