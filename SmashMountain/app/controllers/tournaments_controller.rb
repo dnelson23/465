796bc3a7c3dff4bc91fce794599b2439ba2dc679
Drew Nelson <drwnlsn@gmail.com>
2015-12-11 03:31:04 -0800

    Changes all a tags to link_to, because rails is stupid

--- SmashMountain/app/controllers/tournaments_controller.rb
+++ SmashMountain/app/controllers/tournaments_controller.rb
@@ -15,10 +15,6 @@ class TournamentsController < ApplicationController
         @tournament = JSON.parse(getTournament tournament)
         @tournament = @tournament["tournament"]
 
-        if !Tournament.find_by(name: @tournament["name"]).nil? 
-            redirect_to :back, danger: "Tournament and matches are already in the database."
-        end
-
         @smashers = @tournament["participants"]
         @matches = @tournament["matches"]
     end
@@ -27,36 +23,38 @@ class TournamentsController < ApplicationController
         thisTournament = Tournament.find_or_initialize_by(challonge_id: params[:tournament_id])
         if thisTournament.persisted?
             flash[:danger] = "Tournament is already in the database"
-            redirect_to :back
-        end
-        thisTournament.name = params[:tournament_name]
-        thisTournament.save
+            redirect_to :back 
+            return
+        else
+            thisTournament.name = params[:tournament_name]
+            thisTournament.save
 
-        smashersParams = params[:smashers]
-        matchesParams = params[:matches]
+            smashersParams = params[:smashers]
+            matchesParams = params[:matches]
 
-        smashers = Array.new
-        matches = Array.new
+            smashers = Array.new
+            matches = Array.new
 
-        smashersParams.each do |smasher|
-            thisSmasher = Smasher.find_or_create_by!(tag: smasher["name"])
-            smashers.push({ "challonge_id" => smasher["id"], "smasherObj" => thisSmasher })
-        end
-
-        matchesParams.each do |match|
-            thisMatch = Match.new
-            thisMatch.tournament_id = thisTournament.id
-            smashers.each do |smasher|
-                if smasher["challonge_id"] == match["winner_id"]
-                    thisMatch.winner_id = smasher["smasherObj"].id
-                elsif smasher["challonge_id"] == match["loser_id"]
-                    thisMatch.loser_id = smasher["smasherObj"].id
-                end
+            smashersParams.each do |smasher|
+                thisSmasher = Smasher.find_or_create_by!(tag: smasher["name"])
+                smashers.push({ "challonge_id" => smasher["id"], "smasherObj" => thisSmasher })
             end
-            thisMatch.save
-        end
 
-        flash[:success] = "Tournament and matches uploaded successfully!"
-        render "upload"
+            matchesParams.each do |match|
+                thisMatch = Match.new
+                thisMatch.tournament_id = thisTournament.id
+                smashers.each do |smasher|
+                    if smasher["challonge_id"] == match["winner_id"]
+                        thisMatch.winner_id = smasher["smasherObj"].id
+                    elsif smasher["challonge_id"] == match["loser_id"]
+                        thisMatch.loser_id = smasher["smasherObj"].id
+                    end
+                end
+                thisMatch.save
+            end
+
+            flash[:success] = "Tournament and matches uploaded successfully!"
+            render "upload"
+        end
     end
 end
