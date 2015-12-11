83a42bc0d441d7a008ef2ba854ae46d5be54295a
Drew Nelson <drwnlsn@gmail.com>
2015-12-11 14:48:17 -0800

    Updates config file

--- SmashMountain/config/application.rb
+++ SmashMountain/config/application.rb
@@ -23,5 +23,6 @@ module SmashMountain
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/lib)
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
